62fd322407b2f27f2e92de2bcfb1f974f02a4021
unknown <raffyamoguis@gmail.com>
2022-11-03 13:09:07 +0800

    remove service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 // import reportWebVitals from './reportWebVitals';
-// import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,4 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
-// serviceWorker.register();
