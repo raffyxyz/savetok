ca8b9da216b9835e0b3522d51328f43680c86d4b
unknown <raffyamoguis@gmail.com>
2022-11-04 11:51:58 +0800

    added about info

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,5 +1,17 @@
 import React, { useState } from 'react';
-import { Drawer, Group, Button, Anchor } from '@mantine/core';
+import {
+  Drawer,
+  Group,
+  Button,
+  Anchor,
+  Title,
+  Text,
+  Space,
+  Divider,
+  List,
+  ThemeIcon,
+} from '@mantine/core';
+import { IconCheck, IconHeart } from '@tabler/icons';
 
 const AppHeader = () => {
   const [opened, setOpened] = useState(false);
@@ -18,7 +30,48 @@ const AppHeader = () => {
           position='right'
           size='xl'
         >
-          {/* Drawer content */}
+          <Divider my='sm' />
+          <Title order={2} color='grape'>
+            SaveTok
+          </Title>
+          <Text>Tiktok Video Downloader</Text>
+          <Space h='lg' />
+          <Text>
+            A web app that downloads tiktok videos without the annoying tiktok
+            watermark.
+          </Text>
+          <Space h='lg' />
+          <Title order={5}>Why you should use this kind of service?</Title>
+          <Text>
+            Use this service to repost tiktok videos on other social media sites
+            without the annoying watermark
+          </Text>
+          <Space h='lg' />
+          <Title order={5}>How to use the service?</Title>
+          <Text>
+            Just paste tiktok video link in the textbox and click getVideo, wait
+            for it to load the video info and download button.
+          </Text>
+          <Space h='lg' />
+          <Title order={5}>Tech used</Title>
+          <List
+            spacing='xs'
+            size='sm'
+            mt='xs'
+            center
+            icon={
+              <ThemeIcon color='teal' size={16} radius='xl'>
+                <IconCheck size={14} />
+              </ThemeIcon>
+            }
+          >
+            <List.Item>React.js a frontend js library</List.Item>
+            <List.Item>Mantine a frontend components framework</List.Item>
+            <List.Item>Tiktokwm Api</List.Item>
+          </List>
+          <Space h='xl' />
+          <Title order={5}>Developed by</Title>
+          <Text>Raffy Amoguis a self taught web developer.</Text>
         </Drawer>
         <Button variant='light' color='grape' onClick={() => setOpened(true)}>
           About
